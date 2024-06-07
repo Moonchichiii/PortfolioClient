4c3ce144c550d7b67bf4296b4b24df15e1a5a31b
Mats Gustafsson <113367401+Moonchichiii@users.noreply.github.com>
2024-06-07 15:48:30 +0200

    chore: Remove Redux-Logger middleware from store configuration

--- src/store/Store.jsx
+++ src/store/Store.jsx
@@ -1,18 +1,10 @@
-// store/Store.js
-
-import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
-import { createLogger } from 'redux-logger'; // Import Redux-Logger
+import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../pages/auth/authSlice';
 
-// Create Redux-Logger middleware
-const loggerMiddleware = createLogger();
-
-// Configure Redux store with Redux-Logger middleware
 const store = configureStore({
   reducer: {
     auth: authReducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(loggerMiddleware), 
 });
 
 export default store;
