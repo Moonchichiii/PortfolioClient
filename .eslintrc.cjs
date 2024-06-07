76224591f0a5dcbc343948a16dbecca20ed8e4f5
Mats Gustafsson <113367401+Moonchichiii@users.noreply.github.com>
2024-06-07 13:12:45 +0200

    chore: Update Prettier configuration and add Terser dependency

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,32 +1,32 @@
 module.exports = {
-  "extends": [
+  extends: [
     "airbnb",
     "plugin:react/recommended",
     "plugin:react-hooks/recommended",
     "plugin:jsx-a11y/recommended",
     "plugin:prettier/recommended"
   ],
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     }
   },
-  "env": {
-    "browser": true,
-    "node": true
+  env: {
+    browser: true,
+    node: true
   },
-  "plugins": ["react", "react-hooks", "jsx-a11y", "prettier"],
-  "rules": {
+  plugins: ["react", "react-hooks", "jsx-a11y", "prettier"],
+  rules: {
     "prettier/prettier": "error",
-    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
     "react/react-in-jsx-scope": "off",
     "react/prop-types": "off",
     "jsx-a11y/anchor-is-valid": [
       "error",
       {
-        "components": ["Link"],
-        "specialLink": ["to"],
-        "aspects": ["noHref", "invalidHref", "preferButton"]
+        components: ["Link"],
+        specialLink: ["to"],
+        aspects: ["noHref", "invalidHref", "preferButton"]
       }
     ],
     "linebreak-style": ["error", "unix"]
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import purgecss from 'vite-plugin-purgecss';
 
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    modules: {
-      generateScopedName: '[name]__[local]___[hash:base64:5]',
-    },
-  },
+  plugins: [
+    react(),
+    purgecss({
+      content: ['./**/*.html', './src/**/*.jsx', './src/**/*.js', './src/**/*.tsx', './src/**/*.ts'],
+      safelist: [/^fa-/, /^btn-/], 
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+    }),
+  ],
 });
