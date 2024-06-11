a82d8573f76f5041f1f7fe61aa162abb4a849c0e
Mats Gustafsson <113367401+Moonchichiii@users.noreply.github.com>
2024-06-11 15:27:37 +0200

    chore: Remove @fullhuman/postcss-purgecss package and update CSS styles and module dependencies

---
+++ postcss.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: [
+    require('@fullhuman/postcss-purgecss')({
+      content: ['./src/**/*.html', './src/**/*.jsx', './src/**/*.js', './src/**/*.ts', './src/**/*.tsx'],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+    }),
+  ],
+};
