22f7f06fdb8d0575420ef1275730063dac07a093
Moonchichiii <matgus1983@gmail.com>
2024-06-15 10:03:23 +0200

    chore: Update CSS import in ErrorBoundary component

--- src/components/Common/ErrorBoundary.jsx
+++ src/components/Common/ErrorBoundary.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
-import styles from './ErrorBoundary.module.css'; 
+import styles from './errorBoundary.module.css'; 
 
 class ErrorBoundary extends React.Component {
   constructor(props) {
