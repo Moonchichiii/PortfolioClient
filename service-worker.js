caca879ba9ce875b4771aab7cd529d93b8eaf9ad
Moonchichiii <matgus1983@gmail.com>
2024-06-17 09:00:04 +0200

    service worker update, package.json updates.

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,26 @@
 self.addEventListener('install', event => {
-    event.waitUntil(
-      caches.open('portfolio-cache-v1').then(cache => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/favicon.ico',
-          '/manifest.json',
-          '/assets/index-<hash>.js',
-          '/assets/vendor-<hash>.js',
-          '/assets/index-<hash>.css',
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', event => {
-    event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+  event.waitUntil(
+    caches.open('portfolio-cache-v1').then(cache => {
+      return fetch('/asset-manifest.json')
+        .then(response => response.json())
+        .then(assets => {
+          const urlsToCache = [
+            '/',
+            '/index.html',
+            '/favicon.ico',
+            '/manifest.json',
+            ...Object.values(assets),
+          ];
+          return cache.addAll(urlsToCache);
+        });
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
